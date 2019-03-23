5ea8993a833e15054935603a1bcd15a203764d8b
AmulyaVolam <36735285+AmulyaVolam@users.noreply.github.com>
2019-03-23 09:05:00 +0530

    Add files via upload

---
+++ MyConnection.java
@@ -0,0 +1,33 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class MyConnection {
+     public static Connection getConnection(){
+		Connection con=null;
+		try{
+		  Class.forName("oracle.jdbc.driver.OracleDriver");
+		  con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
+     
+        
+       
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } 
+        
+        return con;
+    }
+    
+    
+}
